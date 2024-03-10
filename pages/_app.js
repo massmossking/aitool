94d52cdf1e7b7085bb683affdc14de4f57729003
massmossking <gedevluck@outlook.com>
2024-03-10 22:35:54 +0800

    Add _app.js for Environment setup

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import { ClerkProvider } from '@clerk/clerk-react';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider frontendApi={process.env.NEXT_PUBLIC_CLERK_FRONTEND_API}>
       <Component {...pageProps} />
     </ClerkProvider>
   );
