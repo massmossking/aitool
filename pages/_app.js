b65aa5e0b955f93771caa93b38917b4e2c48f547
massmossking <gedevluck@outlook.com>
2024-03-11 10:54:45 +0800

    Update public key

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import { ClerkProvider } from '@clerk/clerk-react';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ClerkProvider frontendApi={process.env.NEXT_PUBLIC_CLERK_FRONTEND_API}>
+    <ClerkProvider frontendApi={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
       <Component {...pageProps} />
     </ClerkProvider>
   );
